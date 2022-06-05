adb3f4d735cc720248c3b6b1509432954e8cb6c0
Rashad Moore <rjm4god7@gmail.com>
2022-06-05 15:36:14 -0500

    initial commit

---
+++ js.js
@@ -0,0 +1,9 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-List');
+
+
+//Event Listeners
+
+//Functions
